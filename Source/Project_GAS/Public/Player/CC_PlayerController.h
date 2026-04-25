bfe14741bd63ad282ee370ba141ba5fc2741cec2
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-04-25 17:55:00 +0900

    Section 1 - 7 : Player Controller

---
+++ Source/Project_GAS/Public/Player/CC_PlayerController.h
@@ -0,0 +1,42 @@
+// Copyright CrograNM
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CC_PlayerController.generated.h"
+
+/**
+ * 
+ */
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class PROJECT_GAS_API ACC_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void SetupInputComponent() override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	void Jump();
+	void StopJumping();
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+};
